5cfafad9a3edef3587ec5d3dde26586851a81222
Anton <patrin_toxa@mail.ru>
2024-05-04 17:59:30 +0500

    31 errors

--- backend/users/views.py
+++ backend/users/views.py
@@ -58,7 +58,7 @@ class UserViewSet(UserViewSet):
         else:
             return Response(
                 {"errors": "Вы уже подписаны на этого пользователя."},
-                status=status.HTTP_404_NOT_FOUND
+                status=status.HTTP_400_BAD_REQUEST
             )
 
     @subscribe.mapping.delete
